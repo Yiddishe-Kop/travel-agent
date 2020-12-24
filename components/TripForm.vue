13e5250017fa2f3e50e652c1c284ad3767a1fa6c
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-12-25 00:35:17 +0200

    fix: fetch data

---
+++ components/TripForm.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="p-6 bg-gray-200 bg-opacity-75 rounded-lg shadow-md md:w-3/5 md:p-8"
+  >
+    <formulate-form @submit="submit" class="grid grid-cols-12 gap-4">
+      <formulate-input
+        v-model="form.tripType"
+        :options="{
+          return: 'Return',
+          oneWay: 'One Way',
+          multi: 'Multi City',
+        }"
+        type="radio"
+        validation="required"
+        element-class="flex space-x-4 "
+        outer-class="col-span-full"
+      />
+      <vue-select
+        v-model="form.from"
+        :airports="airports"
+        label="From"
+        class="col-span-6"
+      />
+      <vue-select
+        v-model="form.to"
+        :airports="airports"
+        label="To"
+        class="col-span-6"
+      />
+      <formulate-input
+        v-model="form.departAt"
+        label="Depart Date"
+        type="date"
+        validation="required"
+        outer-class="col-span-6 formulate-input"
+      />
+      <formulate-input
+        v-model="form.returnAt"
+        label="Return Date"
+        type="date"
+        validation="required"
+        outer-class="col-span-6 formulate-input"
+      />
+    </formulate-form>
+  </div>
+</template>
+
+<script>
+import VueSelect from "~/components/VueSelect.vue";
+
+export default {
+  name: "TripForm",
+  components: { VueSelect },
+  data() {
+    return {
+      airports: [],
+      form: {},
+    };
+  },
+  methods: {
+    submit() {
+      console.log("Hooray!");
+    },
+  },
+  async mounted() {
+    const res = await this.$axios.get("/airports.json");
+    this.airports = res.data;
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,63 +10,17 @@
       class="relative flex items-center justify-center flex-1 h-screen py-24 bg-no-repeat bg-cover"
       style="background-image: url(https://source.unsplash.com/rf6ywHVkrlY)"
     >
-      <div class="p-6 bg-gray-200 bg-opacity-75 rounded-lg shadow-md md:w-3/5 md:p-8">
-        <formulate-form @submit="submit" class="grid grid-cols-12 gap-4">
-          <formulate-input
-            v-model="form.tripType"
-            :options="{
-              return: 'Return',
-              oneWay: 'One Way',
-              multi: 'Multi City',
-            }"
-            type="radio"
-            validation="required"
-            element-class="flex space-x-4 "
-            outer-class="col-span-full"
-          />
-          <vue-select v-model="form.from" :airports="airports" label="From" class="col-span-6" />
-          <vue-select v-model="form.to" :airports="airports" label="To" class="col-span-6" />
-          <formulate-input
-            v-model="form.departAt"
-            label="Depart Date"
-            type="date"
-            validation="required"
-            outer-class="col-span-6 formulate-input"
-          />
-          <formulate-input
-            v-model="form.returnAt"
-            label="Return Date"
-            type="date"
-            validation="required"
-            outer-class="col-span-6 formulate-input"
-          />
-        </formulate-form>
-      </div>
+      <trip-form />
     </section>
   </div>
 </template>
 
 <script>
-import VueSelect from "~/components/VueSelect.vue";
+import TripForm from "~/components/TripForm.vue";
 
 export default {
   components: {
-    VueSelect,
+    TripForm,
   },
-  data() {
-    return {
-      airports: [],
-      form: {},
-    };
-  },
-  methods: {
-    submit() {
-      console.log("Hooray!");
-    },
-  },
-  async mounted() {
-    const res = await this.$axios.get("/airports.json");
-    this.airports = res.data;
-  }
 };
 </script>
