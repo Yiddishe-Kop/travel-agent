58e1e64978abe6d3d301e2620fc166d96ec2d237
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-12-25 00:15:36 +0200

    fix: data, search & v-select

--- components/VueSelect.vue
+++ components/VueSelect.vue
@@ -14,19 +14,24 @@
     >
       <template v-slot:option="option">
         <slot name="option" :option="option">
-          {{ option.name }}
+          <div class="py-1">
+            <p class="truncate name">{{ option.name }}</p>
+            <p class="truncate city">{{ option.city }}, {{ option.country }}</p>
+            <h3 class="flex items-center code">
+              <svg class="w-5 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M0 0h24v24H0z" stroke="none"></path><path d="M16 10h4a2 2 0 010 4h-4l-4 7H9l2-7H7l-2 2H2l2-4-2-4h3l2 2h4L9 3h3z"></path></svg>
+              <span>{{ option.code }}</span>
+            </h3>
+          </div>
         </slot>
       </template>
       <template v-slot:no-options>
         <slot name="no-options">
-          <p class="py-6">
-            {{ $t("empty") }}
-          </p>
+          <p class="py-6">Type to search airports...</p>
         </slot>
       </template>
       <template v-slot:selected-option="option">
         <slot name="selected-option" :option="option">
-          {{ option.name }}
+          <span class="truncate"><strong>{{ option.code }}</strong> - {{ option.name }}</span>
         </slot>
       </template>
     </v-select>
@@ -42,6 +47,7 @@ export default {
   props: {
     label: String,
     value: Object | Array,
+    airports: Array,
     multiple: {
       type: Boolean,
       default: false,
@@ -66,14 +72,22 @@ export default {
     }, 350),
     async fetchOptions(search, loading, vm) {
       loading(true);
-
+      vm.searchResults = vm.airports
+        .filter((airport) => {
+          let regex = new RegExp(search, "i");
+          return (
+            regex.test(airport.name) ||
+            regex.test(airport.city) ||
+            regex.test(airport.country) ||
+            regex.test(airport.code)
+          );
+        })
+        .slice(0, 10);
       loading(false);
     },
   },
   async mounted() {
     this.onSearch("", undefined, false);
-    const res = await this.$axios.get("/airports.json");
-    this.searchResults = res.data;
   },
 };
 </script>
@@ -81,9 +95,12 @@ export default {
 <style lang="scss">
 .custom-select {
   .v-select {
-    @apply border-gray-200 border;
     border-radius: 8px;
-    max-width: 360px;
+    background-color: #efefef;
+
+    .vs__search {
+      @apply py-1;
+    }
     &.vs--open {
       border-radius: 8px 8px 0 0;
       min-width: 120px;
@@ -115,9 +132,25 @@ export default {
       @apply bg-gray-100;
       .vs__dropdown-option {
         @apply text-gray-800;
+        .name {
+          @apply font-bold;
+        }
+        .city {
+          @apply text-sm text-gray-500;
+        }
+        .code {
+          @apply text-xl font-black;
+        }
         // color: var(--bg-100) !important;
         &--highlight {
           @apply bg-gray-600 text-gray-100;
+          .name {
+          }
+          .city {
+            @apply text-gray-300;
+          }
+          .code {
+          }
         }
       }
     }
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="w-screen min-h-screen flex flex-col">
-    <header class="py-3 px-6 bg-gray-700">
+  <div class="flex flex-col w-screen min-h-screen">
+    <header class="px-6 py-3 bg-gray-700">
       <a href="/">
-        <h2 class="font-bold text-2xl text-white">High End Travel</h2>
+        <h2 class="text-2xl font-bold text-white">High End Travel</h2>
       </a>
     </header>
 
     <section
-      class="h-screen relative py-24 flex-1 flex items-center justify-center bg-no-repeat bg-cover"
+      class="relative flex items-center justify-center flex-1 h-screen py-24 bg-no-repeat bg-cover"
       style="background-image: url(https://source.unsplash.com/rf6ywHVkrlY)"
     >
-      <div class="bg-gray-200 p-6 md:p-8 rounded-lg shadow-md bg-opacity-75">
-        <formulate-form @submit="submit" class="grid grid-cols-12 gap-x-4">
+      <div class="p-6 bg-gray-200 bg-opacity-75 rounded-lg shadow-md md:w-3/5 md:p-8">
+        <formulate-form @submit="submit" class="grid grid-cols-12 gap-4">
           <formulate-input
             v-model="form.tripType"
             :options="{
@@ -24,9 +24,8 @@
             element-class="flex space-x-4 "
             outer-class="col-span-full"
           />
-          <vue-select v-model="form.from" label="From" class="col-span-6" />
-          <vue-select v-model="form.to" label="To" class="col-span-6" />
-
+          <vue-select v-model="form.from" :airports="airports" label="From" class="col-span-6" />
+          <vue-select v-model="form.to" :airports="airports" label="To" class="col-span-6" />
           <formulate-input
             v-model="form.departAt"
             label="Depart Date"
@@ -56,6 +55,7 @@ export default {
   },
   data() {
     return {
+      airports: [],
       form: {},
     };
   },
@@ -64,5 +64,9 @@ export default {
       console.log("Hooray!");
     },
   },
+  async mounted() {
+    const res = await this.$axios.get("/airports.json");
+    this.airports = res.data;
+  }
 };
 </script>
